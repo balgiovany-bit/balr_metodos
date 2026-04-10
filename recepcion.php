519685cb6b3bb5295f22152f6e1b201a6431f8d9
Giovany Bal Roquel <balgiovany@gmail.com>
2026-04-10 14:08:49 -0600

    finalizacion de la reccepcion

--- recepcion.php
+++ recepcion.php
@@ -8,9 +8,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($nombre) || empty($apellido) || $edad <= 0) {
         echo "<p>Error: todos los campos son obligartorios.</p>";
     exit;}
-}
+
 
 $letrasnombre = strlen($nombre);
 $letrasapellido = strlen($apellido);
 $nombremayuscula = strtoupper($nombre);
 $edadinvertida = strrev(strval($edad));
+
+echo "<h2>Resultados:</h2>";
+echo "<p>Nombre: " . $nombre . " tiene " . $letrasnombre . " letras.</p>";
+echo "<p>Apellido: " . $apellido . " tiene " . $letrasapellido . " letras.</p>";
+echo "<p>Edad: " . $edad . " al reves es: " . $edadinvertida .  "</p>";
+echo "<p>Nombre en mayuscula: " . $nombremayuscula . "</p>";
+
+} else {
+    echo "acceso no permitido.";
+}
+?>
